c35a56e65016e9b0bbf50a3e3bf4ff0525528c9d
tomzacchia <tomzacchia@live.com>
2022-01-04 11:39:36 -0500

    Add bubble sort visualization

--- src/Sort.js
+++ src/Sort.js
@@ -4,18 +4,37 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
-  // do cool stuff here
+function sort(nums) {
+  // code goes here
+  let hasSwapBeenMade = true;
 
-  // call snapshot any time you do anything to the array
-  // it's okay if you call it with duplicate value array,
-  // it will deduplicate for you
-  snapshot(array);
+  // 1. loop while isSorted is false
+  do {
+    hasSwapBeenMade = false;
+
+    // 2. compare two index values
+    for (let i = 0; i < nums.length; i++) {
+      snapshot(nums);
+      let isEndOfArray = i === nums.length - 1;
+      if (isEndOfArray) break;
+
+      let current = nums[i];
+      let next = nums[i + 1];
+
+      if (current > next) {
+        nums[i] = next;
+        nums[i + 1] = current;
+        hasSwapBeenMade = true;
+      }
+    }
+  } while (hasSwapBeenMade);
+
+  snapshot(nums);
 }
 
 export default function SortComponent() {
   clear();
-  sort(shuffle(range(10)));
+  sort(shuffle(range(20)));
   done();
   return <App />;
 }
